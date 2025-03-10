92c68b4cc079aa83fb879d9b2af1647ecc668f1a
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-09 22:38:28 -0300

    feat: env validation

---
+++ src/infrastructure/config/env.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  SERVER_PORT: z.coerce.number().default(3333),
+  MONGO_URL: z
+    .string()
+    .default(
+      'mongodb://admin:example@localhost:27017/oz-tech-test?authSource=admin',
+    ),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.log('🔴 Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
