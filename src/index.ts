495e48aa878f14df2bc4d272a20a01ffcf69cfef
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-09 22:37:19 -0300

    refactor: index start server and database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-import './database';
+import { DatabaseConnection } from './core/database/DatabaseConnection'
+import { MongoDatabase } from './core/database/MongoDatabase'
+import { ExpressServer } from './core/server/ExpressServer'
+import { env } from './infrastructure/config/env'
+import { App } from './infrastructure/App'
+import { UserRoutes } from './infrastructure/routes/UserRoute'
+
+const database = new MongoDatabase(env.MONGO_URL)
+const databaseConnection = new DatabaseConnection(database)
+
+const server = new ExpressServer(env.SERVER_PORT)
+server.configureRoutes([new UserRoutes()])
+
+const app = new App(databaseConnection, server)
+
+app.start()
