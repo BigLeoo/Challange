2adec4ddd124bbd67d0e6080495f0002362a404d
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-11 23:42:55 -0300

    feat: implement create and fetch methods

--- src/domain/application/repositories/RegionRepository.ts
+++ src/domain/application/repositories/RegionRepository.ts
@@ -1,1 +1,6 @@
-export interface RegionRepository {}
+import { Polygon, Region } from '@/domain/enterprise/models/Region'
+
+export interface RegionRepository {
+  create(name: string, polygon: Polygon, userId: string): Promise<void>
+  fetch(): Promise<Region[]>
+}
