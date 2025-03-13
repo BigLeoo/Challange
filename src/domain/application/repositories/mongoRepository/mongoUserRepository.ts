ab7b8281e5afe7f18215870e19755e37bcc5e733
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-12 22:08:42 -0300

    feat: implement mongo use repository

---
+++ src/domain/application/repositories/mongoRepository/mongoUserRepository.ts
@@ -0,0 +1,66 @@
+import { User, UserModel } from '@/domain/enterprise/models/User'
+import { UserRepository } from '../UserRepository'
+
+export class MongoUserRepository implements UserRepository {
+  public async create(
+    name: string,
+    email: string,
+    address: string,
+    coordinates: [number, number],
+  ): Promise<void> {
+    await UserModel.create({ name, email, address, coordinates })
+    const session = await UserModel.startSession()
+    try {
+      session.startTransaction()
+      await UserModel.create({ name, email, address, coordinates })
+      await session.commitTransaction()
+    } catch (error) {
+      await session.abortTransaction()
+      throw error
+    } finally {
+      session.endSession()
+    }
+  }
+
+  public async getByEmail(email: string): Promise<User | null> {
+    return await UserModel.findOne({ email }).lean()
+  }
+
+  public async getById(userId: string): Promise<User | null> {
+    return await UserModel.findById(userId).lean()
+  }
+
+  public async delete(userId: string): Promise<void> {
+    await UserModel.findByIdAndDelete(userId).lean()
+    const session = await UserModel.startSession()
+    try {
+      session.startTransaction()
+      await UserModel.findByIdAndDelete(userId).lean()
+      await session.commitTransaction()
+    } catch (error) {
+      await session.abortTransaction()
+      throw error
+    } finally {
+      session.endSession()
+    }
+  }
+
+  public async fetch(): Promise<User[]> {
+    return await UserModel.find().lean()
+  }
+
+  public async save(user: User): Promise<void> {
+    await UserModel.findByIdAndUpdate(user._id, user).lean()
+    const session = await UserModel.startSession()
+    try {
+      session.startTransaction()
+      await UserModel.findByIdAndUpdate(user._id, user).lean()
+      await session.commitTransaction()
+    } catch (error) {
+      await session.abortTransaction()
+      throw error
+    } finally {
+      session.endSession()
+    }
+  }
+}
