0cbb1b2203c65b5b8b98149efb9ee61c0032f594
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-12 22:08:26 -0300

    feat: implement mongo region repository

---
+++ src/domain/application/repositories/mongoRepository/mongoRegionRepository.ts
@@ -0,0 +1,94 @@
+import { Polygon, Region, RegionModel } from '@/domain/enterprise/models/Region'
+import { RegionRepository } from '../RegionRepository'
+
+export class MongoRegionRepository implements RegionRepository {
+  public async create(
+    name: string,
+    polygon: Polygon,
+    userId: string,
+  ): Promise<void> {
+    const session = await RegionModel.startSession()
+    try {
+      session.startTransaction()
+      await RegionModel.create({ name, polygon, user: userId })
+      await session.commitTransaction()
+    } catch (error) {
+      await session.abortTransaction()
+      throw error
+    } finally {
+      session.endSession()
+    }
+  }
+
+  public async fetch(): Promise<Region[]> {
+    return await RegionModel.find().lean()
+  }
+
+  public async getById(regionId: string): Promise<Region | null> {
+    return await RegionModel.findById(regionId).lean()
+  }
+
+  public async deleteById(regionId: string): Promise<void> {
+    await RegionModel.findByIdAndDelete(regionId).lean()
+    const session = await RegionModel.startSession()
+    try {
+      session.startTransaction()
+      await RegionModel.findByIdAndDelete(regionId).lean()
+      await session.commitTransaction()
+    } catch (error) {
+      await session.abortTransaction()
+      throw error
+    } finally {
+      session.endSession()
+    }
+  }
+
+  public async save(region: Region): Promise<void> {
+    await RegionModel.findByIdAndUpdate(region._id, region).lean()
+    const session = await RegionModel.startSession()
+    try {
+      session.startTransaction()
+      await RegionModel.findByIdAndUpdate(region._id, region).lean()
+      await session.commitTransaction()
+    } catch (error) {
+      await session.abortTransaction()
+      throw error
+    } finally {
+      session.endSession()
+    }
+  }
+
+  public async fetchByCoordinates(
+    longitude: number,
+    latitude: number,
+  ): Promise<Region[]> {
+    return await RegionModel.find({
+      polygon: {
+        $geoIntersects: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+        },
+      },
+    }).lean()
+  }
+
+  public async fetchByDistance(
+    longitude: number,
+    latitude: number,
+    maxDistance: number,
+  ): Promise<Region[]> {
+    return await RegionModel.find({
+      polygon: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude],
+          },
+          $maxDistance: maxDistance,
+        },
+      },
+    })
+  }
+}
