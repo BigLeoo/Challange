62d7ccc9e7b8d18c855f5b3861ba409eacc1b601
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-14 01:09:41 -0300

    feat: implement zod validation error

--- src/interface/controller/user/createUser/CreateUserController.ts
+++ src/interface/controller/user/createUser/CreateUserController.ts
@@ -3,29 +3,40 @@ import { StatusCodes } from 'http-status-codes'
 import { z } from 'zod'
 import { CreateUserUseCase } from '../../../../domain/application/use-cases/user/createUser'
 import { UserAlreadyExist } from '../../../../core/errors/errors/UserAlreadyExist'
+import { InvalidInputCombination } from '../../../../core/errors/errors/InvalidInputCombination'
+import { ZodValidationError } from '../../../../core/errors/errors/ZodValidationError'
 
 export class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   async handler(request: Request, response: Response): Promise<Response> {
-    console.log(request.body)
-
-    const registerBodySchema = z.object({
-      name: z.string(),
-      email: z.string().email(),
-      address: z.string().optional(),
-      coordinates: z
-        .array(z.number())
-        .length(2)
-        .transform((val) => [val[0], val[1]] as [number, number])
-        .optional(),
-    })
-
-    const { name, email, address, coordinates } = registerBodySchema.parse(
-      request.body,
-    )
-
     try {
+      const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        address: z.string().optional(),
+        coordinates: z
+          .array(z.number())
+          .length(2)
+          .transform((val) => [val[0], val[1]] as [number, number])
+          .refine(
+            ([longitude, latitude]) =>
+              latitude >= -90 &&
+              latitude <= 90 &&
+              longitude >= -180 &&
+              longitude <= 180,
+            {
+              message:
+                'Longitude deve estar entre -180 e 180 e Latitude deve estar entre -90 e 90 ',
+            },
+          )
+          .optional(),
+      })
+
+      const { name, email, address, coordinates } = registerBodySchema.parse(
+        request.body,
+      )
+
       await this.createUserUseCase.execute({
         name,
         email,
@@ -34,10 +45,18 @@ export class CreateUserController {
       })
     } catch (error) {
       if (error instanceof z.ZodError) {
-        return response.status(StatusCodes.BAD_REQUEST).json({
-          message: 'Erro de validação',
-          errors: error.errors,
+        const { message, formattedErrors } = new ZodValidationError(error)
+
+        return response.status(StatusCodes.BAD_REQUEST).send({
+          message,
+          errors: formattedErrors,
         })
+      }
+
+      if (error instanceof InvalidInputCombination) {
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .send({ message: error.message })
       }
 
       if (error instanceof UserAlreadyExist) {
