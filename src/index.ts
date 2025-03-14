e62aa3ecb38212ad99dd8e36134ba3ad8cbf94f9
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-14 00:40:33 -0300

    refactor: server routes config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
+import 'reflect-metadata'
 import { DatabaseConnection } from './core/database/DatabaseConnection'
 import { MongoDatabase } from './core/database/MongoDatabase'
 import { ExpressServer } from './core/server/ExpressServer'
 import { env } from './infrastructure/config/env'
 import { App } from './infrastructure/App'
-import { UserRoutes } from './infrastructure/routes/UserRoute'
+import userRoute from './interface/routes/user/UserRoute'
 
 const database = new MongoDatabase(env.MONGO_URL)
 const databaseConnection = new DatabaseConnection(database)
 
-const server = new ExpressServer(env.SERVER_PORT)
-server.configureRoutes([new UserRoutes()])
+const server = new ExpressServer(env.SERVER_PORT, [userRoute])
 
 const app = new App(databaseConnection, server)
 
