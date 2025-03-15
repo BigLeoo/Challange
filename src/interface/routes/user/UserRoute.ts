7ff0a0c06957a57418460afb023f5311580ebb80
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-15 13:22:14 -0300

    feat: implement new user routes

--- src/interface/routes/user/UserRoute.ts
+++ src/interface/routes/user/UserRoute.ts
@@ -1,10 +1,15 @@
 import express from 'express'
-
 import createUserRoute from './createUserRoute'
 import fetchUsersRoute from './fetchUsersRoute'
+import getUserRoute from './getUserRoute'
+import updateUserRoute from './updateUserRoute'
+import deleteUserRoute from './deleteUserRoute'
 
 const userRoute = express.Router()
 userRoute.use('/users', createUserRoute)
 userRoute.use('/users', fetchUsersRoute)
+userRoute.use('/users', getUserRoute)
+userRoute.use('/users', updateUserRoute)
+userRoute.use('/users', deleteUserRoute)
 
 export default userRoute
