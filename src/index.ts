077f3c6b8a179b2a97fe4d7a2f9bc5fe1cf6f83f
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-15 15:36:16 -0300

    feat: implemente region routes

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ import { ExpressServer } from './core/server/ExpressServer'
 import { env } from './infrastructure/config/env'
 import { App } from './infrastructure/App'
 import userRoute from './interface/routes/user/UserRoute'
+import regionRoute from './interface/routes/region/RegionRoute'
 
 const database = new MongoDatabase(env.MONGO_URL)
 const databaseConnection = new DatabaseConnection(database)
 
-const server = new ExpressServer(env.SERVER_PORT, [userRoute])
+const server = new ExpressServer(env.SERVER_PORT, [userRoute, regionRoute])
 
 const app = new App(databaseConnection, server)
 
