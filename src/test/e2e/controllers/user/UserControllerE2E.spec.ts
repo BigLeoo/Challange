09bef11cab43897520ad5f318973d97434979e6d
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-15 20:59:02 -0300

    feat: implement all e2e user controller

--- src/test/e2e/controllers/user/UserControllerE2E.spec.ts
+++ src/test/e2e/controllers/user/UserControllerE2E.spec.ts
@@ -57,6 +57,39 @@ describe('CreateUserController', function () {
       })
       .expect(201)
   })
+
+  it('should not able to create user, BAD REQUEST', async () => {
+    await supertest(app)
+      .post('/api/users')
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        address: 'R. Luís Delfino - Centro, Florianópolis - SC, 88015-360',
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+      .expect(400)
+  })
+
+  it('should not able to create user, CONFLICT', async () => {
+    const email = faker.internet.email()
+
+    await supertest(app)
+      .post('/api/users')
+      .send({
+        name: faker.person.fullName(),
+        email,
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+
+    await supertest(app)
+      .post('/api/users')
+      .send({
+        name: faker.person.fullName(),
+        email,
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+      .expect(409)
+  })
 })
 
 describe('FetchUserController', function () {
@@ -110,5 +143,130 @@ describe('GetUserController', function () {
     expect(response.body.user.coordinates).to.deep.equal(
       userCreated.body.user.coordinates,
     )
+  })
+  it('should not be able to get user by id, NOT FOUND', async () => {
+    await supertest(app)
+      .post('/api/users')
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+
+    await supertest(app).get(`/api/users/12313`).send().expect(404)
+  })
+})
+
+describe('DeleteUserController', function () {
+  this.timeout(10000)
+
+  it('should be able to delete user by id', async () => {
+    const userCreated = await supertest(app)
+      .post('/api/users')
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+
+    const userId = userCreated.body.user._id
+
+    await supertest(app).delete(`/api/users/${userId}`).send().expect(204)
+  })
+  it('should not be able to delete user by id, NOT FOUND', async () => {
+    await supertest(app)
+      .post('/api/users')
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+
+    await supertest(app).delete(`/api/users/123`).send().expect(404)
+  })
+})
+
+describe('UpdateUserController', function () {
+  this.timeout(10000)
+
+  it('should be able to update user, using coordinates', async () => {
+    const userCreated = await supertest(app)
+      .post('/api/users')
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+
+    const userId = userCreated.body.user._id
+
+    await supertest(app)
+      .put(`/api/users/${userId}`)
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+      .expect(200)
+  })
+
+  it('should be able to update user, using address', async () => {
+    const userCreated = await supertest(app).post('/api/users').send({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      address: 'R. Luís Delfino - Centro, Florianópolis - SC, 88015-360',
+    })
+
+    const userId = userCreated.body.user._id
+
+    await supertest(app)
+      .put(`/api/users/${userId}`)
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+      .expect(200)
+  })
+
+  it('should not be able to update user, BAD REQUEST', async () => {
+    const userCreated = await supertest(app).post('/api/users').send({
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      address: 'R. Luís Delfino - Centro, Florianópolis - SC, 88015-360',
+    })
+
+    const userId = userCreated.body.user._id
+
+    await supertest(app)
+      .put(`/api/users/${userId}`)
+      .send({
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        address:
+          'R. Eng. Agronômico Andrei Cristian Ferreira, 2-116 - Carvoeira',
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+      .expect(400)
+  })
+  it('should not be able to update user, CONFLICT', async () => {
+    const email = faker.internet.email()
+
+    const userCreated = await supertest(app).post('/api/users').send({
+      name: faker.person.fullName(),
+      email,
+      address: 'R. Luís Delfino - Centro, Florianópolis - SC, 88015-360',
+    })
+
+    const userId = userCreated.body.user._id
+
+    await supertest(app)
+      .put(`/api/users/${userId}`)
+      .send({
+        name: faker.person.fullName(),
+        email,
+        coordinates: [faker.location.longitude(), faker.location.latitude()],
+      })
+      .expect(409)
   })
 })
