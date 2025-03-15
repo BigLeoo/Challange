43cc6e2facd320a05aba36a28ecb55d15178e9c1
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-15 17:21:45 -0300

    feat: implement new routes

--- src/interface/routes/region/RegionRoute.ts
+++ src/interface/routes/region/RegionRoute.ts
@@ -4,6 +4,8 @@ import getRegionRoute from './getRegionRoute'
 import fetchRegionsRoute from './fetchRegionRoute'
 import updateRegionRoute from './updateRegionRoute'
 import deleteRegionRoute from './deleteRegionRoute'
+import fetchRegionByCoordinatesRoute from './fetchRegionByCoordinatesRoute'
+import fetchRegionByDistanceRoute from './fetchRegionByDistanceRoute'
 
 const regionRoute = express.Router()
 regionRoute.use('/regions', createRegionRoute)
@@ -11,5 +13,7 @@ regionRoute.use('/regions', getRegionRoute)
 regionRoute.use('/regions', fetchRegionsRoute)
 regionRoute.use('/regions', updateRegionRoute)
 regionRoute.use('/regions', deleteRegionRoute)
+regionRoute.use('/regions', fetchRegionByCoordinatesRoute)
+regionRoute.use('/regions', fetchRegionByDistanceRoute)
 
 export default regionRoute
