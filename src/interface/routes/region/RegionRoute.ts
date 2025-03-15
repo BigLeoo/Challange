fb754da8572dafb741e56f9b70ebdada23fc313b
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-15 16:25:30 -0300

    feat: implement new region routes

--- src/interface/routes/region/RegionRoute.ts
+++ src/interface/routes/region/RegionRoute.ts
@@ -2,10 +2,14 @@ import express from 'express'
 import createRegionRoute from './createRegionRoute'
 import getRegionRoute from './getRegionRoute'
 import fetchRegionsRoute from './fetchRegionRoute'
+import updateRegionRoute from './updateRegionRoute'
+import deleteRegionRoute from './deleteRegionRoute'
 
 const regionRoute = express.Router()
 regionRoute.use('/regions', createRegionRoute)
 regionRoute.use('/regions', getRegionRoute)
 regionRoute.use('/regions', fetchRegionsRoute)
+regionRoute.use('/regions', updateRegionRoute)
+regionRoute.use('/regions', deleteRegionRoute)
 
 export default regionRoute
