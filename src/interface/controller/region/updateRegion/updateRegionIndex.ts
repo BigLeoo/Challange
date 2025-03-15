1edba436d8df72f76ab3a282084dd73adf696ddd
BigLeoo <leonardo.v.m.ramoss@gmail.com>
2025-03-15 17:18:42 -0300

    fix: imports

--- src/interface/controller/region/updateRegion/updateRegionIndex.ts
+++ src/interface/controller/region/updateRegion/updateRegionIndex.ts
@@ -1,7 +1,7 @@
 import { MongoRegionRepository } from '../../../../domain/application/repositories/mongoRepository/mongoRegionRepository'
 import { RegionRepository } from '../../../../domain/application/repositories/RegionRepository'
 import { UpdateRegionUseCase } from '../../../../domain/application/use-cases/region/updateRegion'
-import { UpdateRegionController } from './updateRegionController'
+import { UpdateRegionController } from './UpdateRegionController'
 
 const regionRepository: RegionRepository = new MongoRegionRepository()
 const updateRegionUseCase: UpdateRegionUseCase = new UpdateRegionUseCase(
